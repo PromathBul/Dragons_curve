19815bd4499dc655761918fab09e2c5514f008a0
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-11-10 16:10:52 +0300

    Программа выводит изобращения кривой дракона (дракон Хартера)

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+def dracons_curve(txt, n):
+  if n < 0:
+    return ''
+  elif n == 0:
+    return txt.replace('a', '').replace('b', '')
+  else:
+    return dracons_curve(''.join(('aRbFR' if i == 'a' else ('LFaLb' if i == 'b' else i)) for i in txt), n - 1)
+
+def draw (txt):
+  # Установка курсора для n = 15 и шага со значением 2
+  # penup()
+  # goto(0, -200)
+  # pendown()
+  bgcolor('black')
+  color('red')
+  speed(10)
+  for item in txt:
+      if item == 'L':
+        left(90)
+      elif item == 'R':
+        right(90)
+      else:
+        forward(8)
+  done()
+
+txt = dracons_curve('Fa', 9)
+draw(txt)
